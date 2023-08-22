d36c28ccda4a3783b6ac5879ed142c2842ebff02
Jmar33 <josimarferreira922@yahoo.com.br>
2023-08-22 12:09:39 -0300

    [melhoria] Trabalha de forma assincrona usando o then

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+import fs from 'fs'
 import chalk from 'chalk'
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo nesse diretório'))
+}
 
-console.log(chalk.blue('olá mundo'));
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+
+/*
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto))
+    })
+}*/
+
+pegaArquivo('./arquivos/texto.md')
