412697c1e7b9735b36880ccb4c592d3e79d59852
Jmar33 <josimarferreira922@yahoo.com.br>
2023-08-22 13:02:01 -0300

    [melhoria] Trabalha com async/await

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo nesse diretório'))
 }
 
+//async/await
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const enconding = 'utf-8'
+        const resultado = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        console.log(chalk.green(resultado))
+    }catch(erro){
+        trataErro(erro)
+    }
+}
+
+
+
+//then
+/* 
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8'
     fs.promises
         .readFile(caminhoDoArquivo, encoding)
         .then((texto) => console.log(chalk.green(texto)))
         .catch(trataErro)
-}
+}*/
 
 /*
 function pegaArquivo(caminhoDoArquivo){
@@ -25,3 +40,4 @@ function pegaArquivo(caminhoDoArquivo){
 }*/
 
 pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/')
