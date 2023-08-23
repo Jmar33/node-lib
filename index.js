0b5b48503de5c8cf7bfae22c52691209d15388b4
Jmar33 <josimarferreira922@yahoo.com.br>
2023-08-23 14:01:46 -0300

    [feature] Extrai links utilizando regex

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import fs from 'fs'
 import chalk from 'chalk'
 
+
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)]
+    //Para podemos usar o valor de item[posicao] como uma chave de um objeto, devemos envolver dentro de colchetes
+    // da seguinte forma [item[posicao]]
+    const resultado = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    console.log(resultado)
+}
+
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo nesse diretório'))
 }
@@ -9,11 +22,14 @@ function trataErro(erro){
 async function pegaArquivo(caminhoDoArquivo){
     try{
         const enconding = 'utf-8'
-        const resultado = await fs.promises.readFile(caminhoDoArquivo, enconding)
-        console.log(chalk.green(resultado))
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        extraiLinks(texto)
     }catch(erro){
         trataErro(erro)
+    }finally{
+        console.log(chalk.yellow('operação concluída'));
     }
+
 }
 
 
@@ -40,4 +56,3 @@ function pegaArquivo(caminhoDoArquivo){
 }*/
 
 pegaArquivo('./arquivos/texto.md')
-pegaArquivo('./arquivos/')
